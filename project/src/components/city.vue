1d424a255ec31a1cffbb8de856739fa9a4bdc959
gxda <514036333@qq.com>
2019-12-18 11:35:42 +0800

    修改城市

--- project/src/components/city.vue
+++ project/src/components/city.vue
@@ -1,8 +1,20 @@
 <template id="city">
-  <div class="city_box">
-    <div class="city">
-      <!-- 所有的城市 -->
-      <div v-for="(item,index) in city" :key="index">{{item.nm}}</div>
+  <div class="city">
+    <div class="city_box">
+      <div class="wraper">
+        <!-- 左侧的城市列表 -->
+        <ul>
+          <li v-for="(item,index) in city" :key="index" ref="test">
+            <div class="title">{{item.title}}</div>
+            <ol>
+              <!-- 循环城市列表 -->
+              <li v-for="(sitem, index) in item.list" :key="index">
+                <p>{{sitem.nm}}</p>
+              </li>
+            </ol>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -10,29 +22,60 @@
 <script>
 import { getcity } from "../api/index";
 import { handlecitydata } from "../utils/city";
+import Bs from "better-scroll";
 export default {
   data() {
     return {
       city: []
     };
   },
+
   created() {
     getcity().then(data => {
       this.city = handlecitydata(data.cts);
     });
+  },
+  methods: {
+    initBS() {
+      new Bs(".city_box");
+    }
   }
 };
 </script>
 
 <style lang="less">
 @import url("../style/index.less");
-.city_box {
+.city {
   position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-  background: red;
   z-index: 5;
+
+  .city_box {
+    .w(375);
+    height: 100%;
+    overflow: scroll;
+    .wraper {
+      ul {
+        li {
+          background-color: #ddd;
+          font-size: @fs_xxxl;
+          p {
+            .h(45);
+            .l_h(45);
+            font-size: @fs_l;
+            border-bottom: 1px solid #c8c7cc;
+            background-color: #ebebeb;
+            color: #333;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
--- project/src/utils/city.js
+++ project/src/utils/city.js
@@ -1,4 +1,64 @@
 export const handlecitydata = (data) => {
-    console.log(data);
-    return data;
+    let date = {
+        hot: {
+            title: "热门",
+            list: []
+        }
+    };
+    for (let index = 0; index < data.length; index++) {
+        let {
+            py,
+            nm,
+            id
+        } = data[index];
+        let title = py.substr(0, 1).toLocaleUpperCase()
+        data[index].title = title;
+        // console.log(data)
+        if (index < 10) {
+            date.hot.list.push({
+                nm,
+                py,
+                title
+            });
+        }
+
+        if (date[title]) {
+            //有数据做list.push
+            date[title].list.push({
+                id,
+                nm,
+                py
+            });
+        } else {
+            //没有数据做添加
+            date[title] = {
+                title: title,
+                list: [{
+                    id,
+                    nm,
+                    py
+                }]
+            };
+        }
+        // console.log(date);
+
+
+    }
+
+    //排序
+    let hot = [date.hot];
+    console.log(hot);
+    let other = [];
+    for (const key in date) {
+        if (key !== "hot" && key !== "9") {
+            other.push(date[key]);
+        }
+    }
+    let sort = other.sort((a, b) => {
+        return a.title.charCodeAt() - b.title.charCodeAt();
+    });
+    console.log(date);
+    console.log(sort);
+    let reslut = hot.concat(sort);
+    return reslut;
 }
